71374be8683be4b04f35d3bad871bcd9aa0a2e38
mariana <marianaandrade@ua.pt>
2023-03-07 16:23:37 +0000

    Repository interface basic methods

--- Lab3/Lab3_2/CarsService/src/main/java/pt/ua/deti/tqs/CarsService/Data/CarRepository.java
+++ Lab3/Lab3_2/CarsService/src/main/java/pt/ua/deti/tqs/CarsService/Data/CarRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pt.ua.deti.tqs.CarsService.Model.Car;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository  extends JpaRepository<Car, Long> {
+    Car findByCarId(Long carId);
+    List<Car> findAll();
 }
---
+++ Lab3/Lab3_2/CarsService/src/test/java/pt/ua/deti/tqs/CarsService/CarService_UnitTest.java
@@ -0,0 +1,2 @@
+package pt.ua.deti.tqs.CarsService;public class CarService_UnitTest {
+}

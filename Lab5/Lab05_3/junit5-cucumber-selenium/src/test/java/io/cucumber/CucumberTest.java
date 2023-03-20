10f70d82876f692b1afa3f4bfebb00d1216a51b5
mariana <marianaandrade@ua.pt>
2023-03-20 23:58:18 +0000

    junit5-cucumber-selenium done

--- Lab5/Lab05_3/junit5-cucumber-selenium/src/test/java/io/cucumber/CucumberTest.java
+++ Lab5/Lab05_3/junit5-cucumber-selenium/src/test/java/io/cucumber/CucumberTest.java
@@ -1,2 +1,15 @@
-package io.cumcumber;public class CucumberTest {
+package io.cucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/cucumber")
+@ConfigurationParameter(key=GLUE_PROPERTY_NAME, value = "io.cucumber")
+public class CucumberTest {
 }

ea99a325e0ad6c5766f9b5d9399f8131d0be7d37
mariana <marianaandrade@ua.pt>
2023-03-30 19:16:15 +0100

    refactored step up

---
+++ Lab7/Lab07_3/TestContainers/src/test/java/pt/ua/deti/tqs/TestContainers/TestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs.TestContainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
